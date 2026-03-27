ce9d7ef339bdbd6265f6b22dfa2e5b26acb47260
Vedant Wagadre <vedantwagadre52@gmail.com>
2026-03-27 14:57:58 +0530

    Create Insert Interval.py

---
+++ Intervals/Insert Interval.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+
+        result=[]
+        intervals.append(newInterval)
+        intervals.sort()
+      
+        result.append(intervals[0])
+        j=1
+
+
+        while j < len(intervals):
+            if result[-1][1]>=intervals[j][0]:
+                result[-1][1]=max(result[-1][1],intervals[j][1])
+                j+=1
+            else:
+                result.append(intervals[j])
+                j+=1
+
+        return result
